d09b5aaa944f6cb48127629b6e4f2ddf8d158e5a
Mohamed Hisham <m.hisham92@hotmail.com>
2015-12-03 12:09:32 +0200

    fix getting the username in the password reset API

--- data/passwordReset/handlers.js
+++ data/passwordReset/handlers.js
@@ -3,7 +3,7 @@ var Handlers = {};
 
 Handlers.defaultHandler = function(request, reply){
 
-    var username = request.payload.credentialVBO.puid;
+    var username = request.payload.credentialVBO.username;
     var filePath = __dirname+"/"+username+".json";
 
     reply(Util.getData(filePath));
