3edd81c9ea5c4c2b752538400fb846aff153de24
Marwa Hanafy <marwa_hanafy22@yahoo.com>
2016-01-28 11:00:49 +0200

    adding line item api

--- data/routes-index.js
+++ data/routes-index.js
@@ -33,6 +33,7 @@ var routes = [].concat(
     require('./salesOrder_product-get/routes'),
     require('./salesOrder_product-post/routes'),
     require('./salesOrder_orderCreation_POST/routes'),
+    require('./salesOrder-OrderLineItemCreation_POST/routes'),
     require('./salesOrder_orderLineItemSummary_get/routes'),
     require('./customerAccount_customerDataForOrder_put/routes'),
     require('./salesOrder_orderSummary_get/routes'),
---
+++ data/salesOrder-OrderLineItemCreation_POST/routes.js
@@ -0,0 +1,17 @@
+var Handlers = require('./handlers');
+
+var routes = [
+    {
+        method: 'POST',
+        path: '/api/salesOrder-OrderLineItemCreation_POST',
+        config: {
+            handler: Handlers.defaultHandler,
+            description: 'orderCreation API',
+            notes: 'Agent gets order ID',
+            tags: ['api', 'newCustomer', 'orderCreation']
+            //response: {schema: responseModel}
+        }
+    }
+];
+
+module.exports = routes;
