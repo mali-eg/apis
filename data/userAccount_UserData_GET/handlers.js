505af01730999ed746346bf8f6261668be177b49
Mohamed Hisham <m.hisham92@hotmail.com>
2016-02-16 13:46:16 +0200

    fix user_data api

--- data/userAccount_UserData_GET/handlers.js
+++ data/userAccount_UserData_GET/handlers.js
@@ -2,8 +2,8 @@ var Util = require('../../Util');
 var Handlers = {};
 
 Handlers.defaultHandler = function(request, reply){
-    var username = request.query.username;
-    var filePath = __dirname+"/"+username+".json";
+    var username = request.headers.username;
+    var filePath = __dirname+"/data/"+username+".json";
     reply(Util.getData(filePath));
 };
 
