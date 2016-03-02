94a31df5ccbfc2930a7f9d0d6553aa4d910ea02d
Marwa Hanafy <marwa_hanafy22@yahoo.com>
2016-03-02 12:15:46 +0200

    DEFECT: 347

--- data/passwordReset/handlers.js
+++ data/passwordReset/handlers.js
@@ -4,6 +4,7 @@ var Handlers = {};
 Handlers.defaultHandler = function(request, reply){
 
     var puid = request.payload.credentialVBO.puid;
+    console.log(puid);
     var filePath = __dirname+"/"+puid+".json";
 
     reply(Util.getData(filePath));
