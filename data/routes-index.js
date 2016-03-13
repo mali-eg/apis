873747b94268c3386ff52214392660cb75954f09
Mohamed Ghobashy <ghobashy2@gmail.com>
2016-03-13 19:59:35 +0200

    Adding Login Federation stubs

--- data/routes-index.js
+++ data/routes-index.js
@@ -62,7 +62,9 @@ var routes = [].concat(
     require('./subscription_subscriberDetailedView_get/routes'),
     require('./customerAccount_invoice_billedUsage_get/routes'),
     require('./subscription_invoice_billedUsage_get/routes'),
-    require('./invoice_billDocument_get/routes')
+    require('./invoice_billDocument_get/routes'),
+    //federation authentication-user
+    require('./securityToken_Federation_Authentication/routes')
 );
 //console.log("routes:"+routes);
 module.exports = routes;
