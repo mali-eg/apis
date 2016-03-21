2ab1ae6242cfe4598347412102fe6eaa59a4fc5e
Omar Makeen <omar.makeen@vodafone.com>
2016-03-21 18:36:05 +0200

    add new stubs customer details

--- data/subscription_subscriberDetailedView_get/handlers.js
+++ data/subscription_subscriberDetailedView_get/handlers.js
@@ -7,9 +7,8 @@ Handlers.defaultHandler = function (request, reply) {
     var id = '20048765'
     var filePath = __dirname + "/" + id + ".json";
 
-    setTimeout(function () {
-        reply(Util.getData(filePath));
-    }, 2000)
+    reply(Util.getData(filePath));
+
 
 
 };
