49379ea42a06da41a0be293af3bb1e5f10fe3d13
Stani <stanislav.papirny@vodafone.com>
2016-04-14 14:02:48 +0200

    added update order line item

--- data/routes-index.js
+++ data/routes-index.js
@@ -72,7 +72,8 @@ var routes = [].concat(
     require('./securityToken-Invalidation_DELETE/routes'),
 		require('./nba_get/routes'),
     // SSO dashboard redirection
-    require('./securityToken_Federation_Authentication_GET/routes')
+    require('./securityToken_Federation_Authentication_GET/routes'),
+    require('./salesOrder_orderLineItemUpdate_POST/routes')
 
 );
 //console.log("routes:"+routes);
